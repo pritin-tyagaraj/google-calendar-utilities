f5c51cb202f32e12dcf0783ba73a98aa19804507
Pritin Tyagaraj <pritin.cool@gmail.com>
2023-02-18 23:09:43 +0100

    Initial boilerplate

---
+++ Code.ts
@@ -0,0 +1,25 @@
+function doGet() {
+  const form = HtmlService.createTemplateFromFile("view")
+  form.calendars = CalendarApp.getAllCalendars()
+
+  const userProperties = PropertiesService.getUserProperties()
+  form.selectedCalendar = userProperties.getProperty("selectedCalendar")
+  return form.evaluate()
+}
+
+function handleFormSubmit(form) {
+  var calendarId = form.calendar
+  var userProperties = PropertiesService.getUserProperties()
+  userProperties.setProperty("selectedCalendar", calendarId)
+  var calendar = CalendarApp.getCalendarById(calendarId)
+  var events = calendar.getEvents(
+    new Date(),
+    new Date(new Date().setDate(new Date().getDate() + 7))
+  )
+  var result = ""
+  for (var i = 0; i < events.length; i++) {
+    var event = events[i]
+    result += event.getTitle() + "<br />"
+  }
+  return result
+}
