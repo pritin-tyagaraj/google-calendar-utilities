2465a65269148510288a1d94802cfdce9afb1b1b
Pritin Tyagaraj <pritin.cool@gmail.com>
2023-02-22 18:52:52 +0100

    Split view into partials

---
+++ Utils.ts
@@ -0,0 +1,6 @@
+function include(filename, params = {}) {
+  const template = HtmlService.createTemplateFromFile(filename)
+  Object.entries(params).forEach(([key, value]) => (template[key] = value))
+
+  return template.evaluate()
+}
